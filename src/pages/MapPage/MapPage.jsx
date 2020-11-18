06eede373d42dda608cf8ca19587a74382e25f5d
liurrong <ypwenxiang@outlook.com>
2020-11-18 13:33:52 +0800

    add supermap

--- src/pages/MapPage/MapPage.jsx
+++ src/pages/MapPage/MapPage.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import L from 'leaflet';
+import {tiledMapLayer} from '@supermap/iclient-leaflet';
+
 import "./MapPage.less";
 
 function MapPage() {
+
+    useEffect(() => {
+        const url = "https://iserver.supermap.io/iserver/services/map-world/rest/maps/World";
+        const map = L.map('map', {
+            crs: L.CRS.EPSG4326,
+            center: [0, 0],
+            maxZoom: 18,
+            zoom: 1
+        });
+        tiledMapLayer(url).addTo(map);
+    }, [])
+
     return (
-        <div>
-            hello MapPage
+        <div className="map-page">
+            <div className="map-container" id="map" />
         </div>
     )
 }
