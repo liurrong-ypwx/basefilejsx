ebfcc22a105a4c57f72ba2a965aa5cc24c4c36a1
liurrong <ypwenxiang@outlook.com>
2020-11-18 11:29:21 +0800

    add route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './assets/css/App.less';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import TestLess from './pages/TestLess/TestLess';
+import MapPage from './pages/MapPage/MapPage';
 
 function App() {
   return (
-    <div className="App">
-      <TestLess />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact={true} path={"/test"} component={TestLess} />
+        <Route exact={true} path={"/map"} component={MapPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/MapPage/MapPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./MapPage.less";
+
+function MapPage() {
+    return (
+        <div>
+            hello MapPage
+        </div>
+    )
+}
+
+export default MapPage
